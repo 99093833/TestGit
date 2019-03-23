6bfa93c1a5aa314a56fd6123bb3929df3c02863a
Eric.He <99093833@qq.com>
2019-03-23 12:34:53 +0800

    git1 modify RegisterApplication

--- maincloud/register/src/main/java/cn/eric/register/RegisterApplication.java
+++ maincloud/register/src/main/java/cn/eric/register/RegisterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class RegisterApplication {
-
+//git 1 add 
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterApplication.class, args);
 	}
